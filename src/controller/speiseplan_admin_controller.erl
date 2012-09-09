b2832aa4842392f324e0e19c9a500f3f9ff0e1ad
Ulf <uaforum1@googlemail.com>
2012-09-09 19:50:36 +0200

    bug fixing

--- src/controller/speiseplan_admin_controller.erl
+++ src/controller/speiseplan_admin_controller.erl
@@ -25,14 +25,20 @@ delete('POST', [Id], Admin) ->
 	{ok, [{'action', "index"}]}.
 	
 add('POST', [Id], Admin) ->
-	{Y,M,D} = erlang:date(),
+	Date = calendar:universal_time(),
 	EaterId = Req:post_param("esser"),		
 	case boss_db:find(booking, [{menu_id, 'equals', Id}, {eater_id , 'equals', EaterId}]) of
 		[Result] -> 	{redirect, "/admin/detail/" ++ Id};
-		_ ->	NewBooking = booking:new(id, construct_date({Y,M,D}), false, EaterId, Id),
+		_ ->	NewBooking = booking:new(id, Date, false, EaterId, Id),
 				{ok, SavedBooking} = NewBooking:save(),
 				{redirect, "/admin/detail/" ++ Id}
 	end.
+	
+remove('POST', [Id], Admin) ->
+	EaterId = Req:post_param("esser"),
+	[Booking] = boss_db:find(booking, [{menu_id, 'equals', Id}, {eater_id , 'equals', EaterId}]),
+	ok = boss_db:delete(Booking:id()),
+	{redirect, "/admin/detail/" ++ Id}.
 
 storno('POST', [], Admin) ->
 	Menu_Id = Req:post_param("menu-id"),
@@ -50,7 +56,7 @@ remove_bookings([Booking|Bookings]) ->
 	remove_bookings(Bookings).
 	
 create('POST', [], Admin) ->
-	Date = Req:post_param("date"),		
+	Date = Req:post_param("date"),
 	Title = Req:post_param("title"),
 	Details = Req:post_param("details"),
 	Slots = Req:post_param("slots"),
@@ -71,8 +77,8 @@ update('POST', [Id], Admin) ->
 	Details = Req:post_param("details"),
 	Slots = Req:post_param("slots"),
 	Vegetarian = Req:post_param("vegetarian"),	
-	NewDish = Dish:set([{'date', date_lib:create_date_from_string(Date)}, {'title', Title}, {'details', Details}, {'slots', Slots}, {'vegetarian', handle_checkbox(Vegetarian)}]),
-	NewMenu = Menu:set([{'slots', Slots}]),
+	NewDish = Dish:set([{'title', Title}, {'details', Details}, {'vegetarian', handle_checkbox(Vegetarian)}]),
+	NewMenu = Menu:set([{'date', date_lib:create_date_from_string(Date)}, {'slots', Slots}]),
 	{ok, SavedDish} = NewDish:save(),
 	{ok, SavedMenu} = NewMenu:save(),	
 	{redirect, [{'action', "index"}]}.
