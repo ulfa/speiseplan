20392a6e82bcb9307181ebf79d6228828559eed4
Ulf <uaforum1@googlemail.com>
2012-09-09 09:18:05 +0200

    fixed the removed functions from the menu

--- src/model/menu.erl
+++ src/model/menu.erl
@@ -3,3 +3,88 @@
 -belongs_to(dish).
 -has({booking, many}).
 
+
+get_slot_count() ->
+	list_to_integer(Slots) - erlang:length(booking()).
+
+get_all_eater() ->
+	get_a_eater(booking(), []).
+
+get_a_eater([], Eaters) ->
+	Eaters;
+get_a_eater([Booking|Bookings], Eaters) ->
+	Eater = Booking:eater(),
+	get_a_eater(Bookings, [Eater:id()|Eaters]).
+
+get_vegetarian_count() ->
+	get_v_count(booking()).
+
+get_v_count([]) ->
+	0;	
+get_v_count([Booking|Bookings]) ->	
+	get_v_count([Booking|Bookings], 0).
+
+get_v_count([], Count) ->
+			Count;			
+get_v_count([Booking|Bookings], Count) ->
+	get_v_count(Bookings, is_vegetarian(Booking, Count));
+get_v_count(Booking, Count) ->
+	is_vegetarian(Booking, Count).
+
+is_vegetarian([], Count) ->
+		Count;
+is_vegetarian(Booking, Count) ->
+	io:format("vegetarin : ~p~n", [Booking:vegetarian()]),
+	case Booking:vegetarian() of
+		true -> Count + 1;
+		_ -> Count
+	end.
+
+
+get_normal_eater() ->
+	get_n_eater(booking(), []).
+
+get_n_eater([], Eaters) ->
+	Eaters;	
+get_n_eater([Booking|Bookings], Eaters) ->
+	get_n_eater(Bookings, get_norm_eater(Booking, Eaters));
+get_n_eater(Booking, Eaters) ->
+	get_norm_eater(Booking, Eaters).
+			
+get_norm_eater([], Eaters) ->
+	Eaters;
+get_norm_eater(Booking, Eaters) ->
+	case Booking:vegetarian() of
+		false -> Eater = Booking:eater(), [Eater:name()|Eaters];
+		_ -> Eaters
+	end.
+
+	
+get_vegetarian_eater() ->
+	get_v_eater(booking(), []).
+
+get_v_eater([], Eaters) ->
+	Eaters;	
+get_v_eater([Booking|Bookings], Eaters) ->
+	get_v_eater(Bookings, get_vegi_eater(Booking, Eaters));
+get_v_eater(Booking, Eaters) ->
+	get_vegi_eater(Booking, Eaters).
+			
+get_vegi_eater([], Eaters) ->
+	Eaters;
+get_vegi_eater(Booking, Eaters) ->
+	case Booking:vegetarian() of
+		true -> Eater = Booking:eater(), [Eater:name()|Eaters];
+		_ -> Eaters
+	end.
+	
+get_sum() ->
+	get_s(booking(), 0).
+
+get_s([], Sum) ->
+	Sum;
+
+get_s([Booking|Bookings], Sum) ->
+	Eater = Booking:eater(),
+	Sum1 = Sum + Eater:price_to_pay(),
+	get_s(Bookings, Sum1).
