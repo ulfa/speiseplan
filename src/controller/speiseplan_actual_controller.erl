5498dd436f14f5872c6b65231fdb54ffb2445b3c
Ulf <uaforum1@googlemail.com>
2012-10-11 09:57:37 +0200

    removed case statement

--- src/controller/speiseplan_actual_controller.erl
+++ src/controller/speiseplan_actual_controller.erl
@@ -1,10 +1,15 @@
 -module(speiseplan_actual_controller, [Req]).
 -compile(export_all).
 
-index('GET', [], Eater) ->
-	D = {erlang:date(), {0,0,0}},
- 	Date = date_lib:create_date_german_string(D),	
-	case boss_db:find(menu, [{date,  D}]) of
-		[] -> {ok, [{eater, Eater}, {text, "bleibt die K&Uuml;che kalt"}, {date, Date}]};
-		[Menu] -> {ok, [{eater, Eater}, {text, "gibt es"}, {dish, Menu:dish()}, {date, Date}]}
-	end.
+	index('GET', [], Eater) ->
+		D = {erlang:date(), {0,0,0}},
+		Date = date_lib:create_date_german_string(D),	
+		show_menu(boss_db:find(menu, [{date,  D}]), Date, Eater).
+		
+	show_menu([], Date, Eater) ->
+		{ok, [{eater, Eater}, {text, "bleibt die K&Uuml;che kalt"}, {date, Date}]};
+
+	show_menu([Menu], Date, Eater) ->
+		{ok, [{eater, Eater}, {text, "gibt es"}, {dish, Menu:dish()}, {date, Date}]}.
+		
+		
