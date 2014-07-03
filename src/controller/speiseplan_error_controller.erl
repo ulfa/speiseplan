151b90f7886340aa47f4f16f38300d6fe91e75a4
Ulf <uaforum1@googlemail.com>
2014-07-03 06:28:48 +0200

    refactoring of the user and error handling

--- src/controller/speiseplan_error_controller.erl
+++ src/controller/speiseplan_error_controller.erl
@@ -1,6 +1,10 @@
 -module(speiseplan_error_controller, [Req]).
+-compile(export_all).
 
 viernullvier('GET', []) ->
+    {ok, []}.
+
+viernulleins('GET', []) ->
     {ok, []}.
 
 fuenfhundert('GET', []) ->
--- src/lib/user_lib.erl
+++ src/lib/user_lib.erl
@@ -9,12 +9,15 @@ hash_for(Name, Password) ->
   hash_password(Password, Salt).
 
 require_login(Req) ->
-Account = case Req:header("REMOTE_USER") of 
-    undefined -> "ua";
-    Acc -> Acc
-  end,  
-  [E] = boss_db:find(eater, [{account, 'equals', Account}]),
-  {ok, E}.
+    Account = case Req:header("REMOTE_USER") of
+        undefined -> lager:warning("SOMEONE IS ABLE TO LOGIN WITH NO REMOTE HEADER SET"),
+                     "ua";
+        User -> User        
+    end,
+    case boss_db:find(eater, [{account, 'equals', Account}]) of 
+        [E] -> {ok, E};
+        [] -> {redirect, elib:get_full_path(speiseplan, "/error/viernulleins")}
+    end.
 
 require_login(admin, Req) -> 
 	require_login(Req).
